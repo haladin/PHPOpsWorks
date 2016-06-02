7a2dc82ccbf826f67396ddd56499ee12d0afbbc2
Slav Hadzhidimitrov <slavhadji@gmail.com>
2016-06-03 00:50:51 +0300

    archive cron script, typo fixed and added actual App short name -> admin_scripts

--- deploy_scripts/recipes/nfs_sync_cron.rb
+++ deploy_scripts/recipes/nfs_sync_cron.rb
@@ -1,5 +1,5 @@
 Chef::Log.info("Creating cron job for S3 Sync")
-env = Hash["AWS_ACCESS_KEY_ID" => "#{ node[:deploy]['scripts'][:environment_variables][:AWS_ACCESS_KEY_ID]}", "AWS_SECRET_ACCESS_KEY" => "#{ node[:deploy]['scripts'][:environment_variables][:AWS_SECRET_ACCESS_KEY]}", "REGION" => "#{ node[:deploy]['scripts'][:environment_variables][:REGION]}"]
+env = Hash["AWS_ACCESS_KEY_ID" => "#{ node[:deploy]['admin_scripts'][:environment_variables][:AWS_ACCESS_KEY_ID]}", "AWS_SECRET_ACCESS_KEY" => "#{ node[:deploy]['admin_scripts'][:environment_variables][:AWS_SECRET_ACCESS_KEY]}", "REGION" => "#{ node[:deploy]['admin_scripts'][:environment_variables][:REGION]}"]
 
 cron 'S3_sync' do
     user'ec2-user'
@@ -9,6 +9,6 @@ cron 'S3_sync' do
     month '*'
     weekday '*'
     environment env
-    command "aws s3 sync /srv/nfs/export s3://zmoutauto.storage.archive "
+    command "aws s3 sync /srv/nfs/export s3://zmoutauto.storage.archive"
     action :create
 end
