00b41820d3998e7f4e2dda093969794b5174f2db
Jeremy Patterson <jpatterson@zmotauto.com>
2016-06-06 12:18:10 -1000

    Update setenv.rb

--- deploy_scripts/recipes/setenv.rb
+++ deploy_scripts/recipes/setenv.rb
@@ -23,3 +23,16 @@ node[:deploy].each do |application, deploy|
     only_if {File.exists?("#{deploy[:deploy_to]}/shared")}
   end
 end
+
+node[:deploy].each do |application, deploy|
+  template File.join(deploy[:deploy_to], "shared","phpvars.env.php") do
+    source "phpvars.env.erb"
+    mode 0770
+    owner deploy[:user]
+    group deploy[:group]
+    variables(
+      :environment => OpsWorks::Escape.escape_double_quotes(deploy[:environment_variables])
+    )
+    only_if {File.exists?("#{deploy[:deploy_to]}/shared")}
+  end
+end
