9548c57638bfb24b3ac18c27bbcff3eee445fbac
Slav Hadzhidimitrov <slavhadji@gmail.com>
2016-06-07 00:17:30 +0300

    adding perl template

--- Berksfile
+++ Berksfile
@@ -1,3 +1,5 @@
 source 'https://supermarket.chef.io'
 
+cookbook 'build-essential', '= 3.2.0'
 cookbook 'php', '~> 1.9.0'
+cookbook 'nfs', '~> 2.2.8'
--- deploy_scripts/recipes/setenv.rb
+++ deploy_scripts/recipes/setenv.rb
@@ -10,3 +10,16 @@ node[:deploy].each do |application, deploy|
     only_if {File.exists?("#{deploy[:deploy_to]}/shared")}
   end
 end
+
+node[:deploy].each do |application, deploy|
+  template File.join(deploy[:deploy_to], "shared","perlvars.env.pl") do
+    source "perlvars.env.erb"
+    mode 0770
+    owner deploy[:user]
+    group deploy[:group]
+    variables(
+      :environment => OpsWorks::Escape.escape_double_quotes(deploy[:environment_variables])
+    )
+    only_if {File.exists?("#{deploy[:deploy_to]}/shared")}
+  end
+end
